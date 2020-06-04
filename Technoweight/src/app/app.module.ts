5c8f5cd474567ba2440c773eb5a0abe4e6be85f3
SazKum <disazid@gmail.com>
2020-06-04 15:19:09 +0530

    comment for commit

--- Technoweight/src/app/app.module.ts
+++ Technoweight/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Technoweight/src/app/resume/resume.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+    selector: 'app-resume',
+    templateUrl: './resume.component.html',
+    styleUrls: ['./resume.component.css']
+})
+export class ResumeComponent {
+   imgPath = "file:///C:/Users/kumarssa/Documents/MKC/training/main_project/profile_avatar.png";
+    
+}
